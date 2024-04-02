e096e57e7a7453d2490acb9ec28458ae9e22f378
acevaliant <202211070@diu.iiitvadodara.ac.in>
2024-04-02 13:29:51 +0530

    creating db relations using python

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 
 import mysql.connector
 
-passd = ""
+passd = "user123!"
 
 #Creating a Database
 try:
@@ -12,3 +12,14 @@ try:
     cursor.execute("CREATE DATABASE blood_bank")
 except:
     print("Following Database already exists.")
+
+#Creating Tables
+try:
+    db = mysql.connector.connect(host="localhost", user="root", passwd=passd, database="blood_bank")
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE DonorInfo(AadharNo CHAR(12) PRIMARY KEY, Name VARCHAR(20), BloodGroup VARCHAR(5), Amount INT)")
+    cursor.execute("CREATE TABLE PatientsInfo(AadharNo CHAR(12) PRIMARY KEY, Name VARCHAR(20), BloodGroup VARCHAR(5), Amount INT, Disease VARCHAR(20))")
+    cursor.execute("CREATE TABLE OrderInfo(OrderId INT PRIMARY KEY, AadharNo CHAR(12), Status CHAR(1), ProductId INT)")
+    cursor.execute("CREATE TABLE BloodInfo(ProductId INT PRIMARY KEY, BloodGroup VARCHAR(5), NetAmount INT)")
+except:
+    print("Tables Already Created")
